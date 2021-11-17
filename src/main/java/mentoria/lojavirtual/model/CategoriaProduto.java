dbd81e14aab105fbfc844cd67adfeca89b91d5d9
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-11-17 20:01:16 -0300

    Commit classe categoria produto

---
+++ src/main/java/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -0,0 +1,70 @@
+package mentoria.lojavirtual.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria_produto")
+@SequenceGenerator(name="seq_categoria_produto", sequenceName="seq_categoria_produto", allocationSize= 1, initialValue=1)
+public class CategoriaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+	private Long id;
+	
+	@Column(name="nome_desc", nullable = false)
+	private String nomeDesc;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeDesc() {
+		return nomeDesc;
+	}
+
+	public void setNomeDesc(String nomeDesc) {
+		this.nomeDesc = nomeDesc;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaProduto other = (CategoriaProduto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
