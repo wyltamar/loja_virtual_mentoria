37f2f000704a1f92923965a8aa4d8dee8e7e7ff7
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-11-17 19:43:56 -0300

    adicionada o equals hashcode e notação em nomeDesc

--- src/main/java/mentoria/lojavirtual/model/MarcaProduto.java
+++ src/main/java/mentoria/lojavirtual/model/MarcaProduto.java
@@ -2,6 +2,7 @@ package mentoria.lojavirtual.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,7 +20,8 @@ public class MarcaProduto implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
-
+	
+	@Column(name="nome_desc")
 	private String nomeDesc;
 
 	public Long getId() {
@@ -37,6 +39,32 @@ public class MarcaProduto implements Serializable {
 	public void setNomeDesc(String nomeDesc) {
 		this.nomeDesc = nomeDesc;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MarcaProduto other = (MarcaProduto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 	
 	
 
