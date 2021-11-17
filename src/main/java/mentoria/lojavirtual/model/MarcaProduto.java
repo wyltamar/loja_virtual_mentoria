db2c8335e83cf2bc701ded68d56824a0aaef6aed
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-11-17 19:46:02 -0300

    tornando o campo nome_desc obirgatório

--- src/main/java/mentoria/lojavirtual/model/MarcaProduto.java
+++ src/main/java/mentoria/lojavirtual/model/MarcaProduto.java
@@ -21,7 +21,7 @@ public class MarcaProduto implements Serializable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
 	
-	@Column(name="nome_desc")
+	@Column(name="nome_desc", nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
