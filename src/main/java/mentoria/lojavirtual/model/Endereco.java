5824af6a7942b82ca31e04d47cdec1de0870318f
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-11-25 20:08:46 -0300

    Criação do atributo tipoEndereco

--- src/main/java/mentoria/lojavirtual/model/Endereco.java
+++ src/main/java/mentoria/lojavirtual/model/Endereco.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +14,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import mentoria.lojavirtual.enums.TipoEndereco;
 
 
 @Entity
@@ -36,6 +40,17 @@ public class Endereco implements Serializable {
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoEndereco tipoEndereco;
+	
+	public TipoEndereco getTipoEndereco() {
+		return tipoEndereco;
+	}
+	
+	public void setTipoEndereco(TipoEndereco tipoEndereco) {
+		this.tipoEndereco = tipoEndereco;
+	}
 
 	
 	public Long getId() {
