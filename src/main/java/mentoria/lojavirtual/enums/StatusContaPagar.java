a18a8003ab2bdd90d1067085167f77841bfb030d
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-12-03 17:08:00 -0300

    Criação do enum StatusContaPagar

---
+++ src/main/java/mentoria/lojavirtual/enums/StatusContaPagar.java
@@ -0,0 +1,25 @@
+package mentoria.lojavirtual.enums;
+
+public enum StatusContaPagar {
+	
+	COBRANCA ("Pagar"),
+	VENCIDA ("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA ("Quit ada");
+	
+	private String descricao;
+	
+	private StatusContaPagar(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+	
+}
