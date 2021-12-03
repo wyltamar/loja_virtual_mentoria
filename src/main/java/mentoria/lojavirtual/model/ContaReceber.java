b31313a0e1f985f1d8141d087cb58eee8867ef79
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-12-03 17:04:01 -0300

    Conta receber alterada identação

--- src/main/java/mentoria/lojavirtual/model/ContaReceber.java
+++ src/main/java/mentoria/lojavirtual/model/ContaReceber.java
@@ -21,148 +21,98 @@ import javax.persistence.TemporalType;
 
 import mentoria.lojavirtual.enums.StatusContaReceber;
 
-
 @Entity
 @Table(name = "conta_receber")
 @SequenceGenerator(name = "seq_conta_receber", sequenceName = "seq_conta_receber", initialValue = 1, allocationSize = 1)
 public class ContaReceber implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_receber")
 	private Long id;
-	
+
 	private String descricao;
-	
+
 	@Enumerated(EnumType.STRING)
 	private StatusContaReceber status;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date dtVencimento;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date dtPagamento;
-	
+
 	private BigDecimal valorTotal;
-	
+
 	private BigDecimal valorDesconto;
-	
 
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getDescricao() {
 		return descricao;
 	}
-
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
-
 	public StatusContaReceber getStatus() {
 		return status;
 	}
-
 
 	public void setStatus(StatusContaReceber status) {
 		this.status = status;
 	}
 
-
 	public Date getDtVencimento() {
 		return dtVencimento;
 	}
-
 
 	public void setDtVencimento(Date dtVencimento) {
 		this.dtVencimento = dtVencimento;
 	}
 
-
 	public Date getDtPagamento() {
 		return dtPagamento;
 	}
-
 
 	public void setDtPagamento(Date dtPagamento) {
 		this.dtPagamento = dtPagamento;
 	}
 
-
 	public BigDecimal getValorTotal() {
 		return valorTotal;
 	}
-
 
 	public void setValorTotal(BigDecimal valorTotal) {
 		this.valorTotal = valorTotal;
 	}
 
-
 	public BigDecimal getValorDesconto() {
 		return valorDesconto;
 	}
-
 
 	public void setValorDesconto(BigDecimal valorDesconto) {
 		this.valorDesconto = valorDesconto;
 	}
 
-
 	public Pessoa getPessoa() {
 		return pessoa;
 	}
 
-
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ContaReceber other = (ContaReceber) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
-	
-	
-
 
 }
