bda6218baaa77a6e87f6e6aae2b7e7f1c187a584
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-12-15 22:12:15 -0300

    Associação da classe StatusRastreio com a classe VendaCompraLojaVirtual

--- src/main/java/mentoria/lojavirtual/model/StatusRastreio.java
+++ src/main/java/mentoria/lojavirtual/model/StatusRastreio.java
@@ -2,10 +2,14 @@ package mentoria.lojavirtual.model;
 
 import java.io.Serializable;
 
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -27,6 +31,11 @@ public class StatusRastreio implements Serializable {
 	private String estado;
 	
 	private String status;
+	
+	@ManyToOne
+	@JoinColumn(name = "venda_compra_loja_virtual_id", nullable = false, foreignKey = 
+	@ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_compra_loja_virtual_fk" ))
+	private VendaCompraLojaVirtual vendaCompraLojaVirtual;
 
 	public Long getId() {
 		return id;
