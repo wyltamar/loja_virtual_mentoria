2db852390f5b58d7708dc904733647b1a004b3be
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-12-27 21:16:51 -0300

    Revisão da classe Produto

--- src/main/java/mentoria/lojavirtual/model/Produto.java
+++ src/main/java/mentoria/lojavirtual/model/Produto.java
@@ -22,27 +22,36 @@ public class Produto implements Serializable{
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
 	private Long id;
 	
+	@Column(nullable = false)
 	private String tipoUnidade;
 	
+	@Column(nullable = false)
 	private String nome;
 	
+	@Column(nullable = false)
 	private Boolean ativo = Boolean.TRUE;
 	
-	@Column(columnDefinition = "text", length = 2000)
+	@Column(columnDefinition = "text", length = 2000, nullable = false)
 	private String descricao;
 	
 	/*FALTA ITEM NOTA PRODUTO - ASSOCIAR */
 	
+	@Column(nullable = false)
 	private Double peso;
 	
+	@Column(nullable = false)
 	private Double largura;
 	
+	@Column(nullable = false)
 	private Double altura;
 	
+	@Column(nullable = false)
 	private Double profundidade;
 	
+	@Column(nullable = false)
 	private BigDecimal valorVenda = BigDecimal.ZERO;
 	
+	@Column(nullable = false)
 	private Integer qtdEstoque = 0;
 	
 	private Integer qtdAlertaEstoque = 0;
