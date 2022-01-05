543d7857808f2638ee50f5a268027ffe76d78e80
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-01-05 17:10:01 -0300

    Inserções de anotações na classe Application do Spring Boot - ok

--- src/main/java/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,16 @@ package mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "mentoria.lojavirtual.model")
+@ComponentScan(basePackages = {"mentoria.*"})
+@EnableJpaRepositories(basePackages = {"mentoria.lojavirtual.repository"})
+@EnableTransactionManagement
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
