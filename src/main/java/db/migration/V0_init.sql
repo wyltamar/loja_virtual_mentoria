bb602daf57642e70c1f475188010bfe3a0c67f41
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-01-06 21:20:04 -0300

    Correção da V0_init.sql

--- src/main/java/db/migration/V0_init.sql
+++ src/main/java/db/migration/V0_init.sql
@@ -25,7 +25,7 @@ SET row_security = off;
 --CREATE DATABASE loja_virtual_mentoria WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'Portuguese_Brazil.1252' LC_CTYPE = 'Portuguese_Brazil.1252';
 
 
-ALTER DATABASE loja_virtual_mentoria_teste OWNER TO postgres;
+ALTER DATABASE loja_virtual_mentoria OWNER TO postgres;
 
 --\connect loja_virtual_mentoria
 
