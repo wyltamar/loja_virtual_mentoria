1a9f8e873e3c72774b62afad55f6538cb4720434
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-01-07 16:16:08 -0300

    Injeção de dependência acessoRepository no arquivo AcessoService

--- src/main/java/mentoria/lojavirtual/service/AcessoService.java
+++ src/main/java/mentoria/lojavirtual/service/AcessoService.java
@@ -1,10 +1,16 @@
 package mentoria.lojavirtual.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import mentoria.lojavirtual.repository.AcessoRepository;
 
 
 @Service
 public class AcessoService {
+	
+	@Autowired
+	private AcessoRepository acessoRepository;
 	
 }
 
