9afe9107dfd2a6a3a8f35b1fd5dc33ca37c6b3ff
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-01-07 16:24:38 -0300

    Implementação do método save no acessoService chamando a dependência acessoRepository para possibilitar a insersão do acesso no banco

--- src/main/java/mentoria/lojavirtual/service/AcessoService.java
+++ src/main/java/mentoria/lojavirtual/service/AcessoService.java
@@ -3,6 +3,7 @@ package mentoria.lojavirtual.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mentoria.lojavirtual.model.Acesso;
 import mentoria.lojavirtual.repository.AcessoRepository;
 
 
@@ -11,6 +12,14 @@ public class AcessoService {
 	
 	@Autowired
 	private AcessoRepository acessoRepository;
+	
+	public Acesso save(Acesso acesso) {
+		
+		/*Aqui podemos fazer qualque tipo de validação*/
+		
+		
+		return acessoRepository.save(acesso);
+	}
 	
 }
 
