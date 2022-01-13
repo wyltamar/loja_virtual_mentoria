687f01805650b8e7ed3179da427696d51b2e42a9
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-01-13 17:07:24 -0300

    Alteração do AcessoController para testar o endpoint através do postman

--- src/main/java/mentoria/lojavirtual/controller/AcessoController.java
+++ src/main/java/mentoria/lojavirtual/controller/AcessoController.java
@@ -1,21 +1,30 @@
 package mentoria.lojavirtual.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import mentoria.lojavirtual.model.Acesso;
 import mentoria.lojavirtual.service.AcessoService;
 
 @Controller
+@RestController
 public class AcessoController {
 
 	@Autowired
 	private AcessoService acessoService;
 	
-	@PostMapping("/salvarAcesso")
-	public Acesso salvarAcesso(Acesso acesso) {
+	@ResponseBody /*Para ser possível dar um retorno da API*/
+	@PostMapping(value = "**/salvarAcesso") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) { /*Recebe o JSON e converte para Objeto Java*/
 		
-		return acessoService.save(acesso);
+		Acesso acessoSalvo = acessoService.save(acesso);
+		
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
 	}
 }
