162467051e8a3492e16f4b301c541e015344f314
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-02-05 11:12:12 -0300

    Implementação do UserDatailsService

--- src/main/java/mentoria/lojavirtual/service/ImplementacaoUserDetailsService.java
+++ src/main/java/mentoria/lojavirtual/service/ImplementacaoUserDetailsService.java
@@ -1,14 +1,29 @@
 package mentoria.lojavirtual.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import mentoria.lojavirtual.model.Usuario;
+import mentoria.lojavirtual.repository.UsuarioRepository;
+
 public class ImplementacaoUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+		
+		Usuario usuario = usuarioRepository.findUsuarioByLogin(username); /*Recebe o login para consulta*/ 
+		
+		if(usuario == null) {
+			throw new UsernameNotFoundException("Usuário não foi encontrado");
+		}
+		
+		return new User(usuario.getLogin(), usuario.getSenha(), usuario.getAuthorities());
 	}
 
 }
