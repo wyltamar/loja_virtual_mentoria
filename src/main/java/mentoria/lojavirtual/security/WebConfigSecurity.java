24740dff5ca64504e56c5cd8da2cba0952211b04
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-02-06 17:38:13 -0300

    Implementação do método protected void configure que recebe o parâmetro (AuthenticationManagerBuilder auth)

--- src/main/java/mentoria/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/mentoria/lojavirtual/security/WebConfigSecurity.java
@@ -2,17 +2,31 @@ package mentoria.lojavirtual.security;
 
 import javax.servlet.http.HttpSessionListener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import mentoria.lojavirtual.service.ImplementacaoUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
+
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+	
+	/*Irá consultar o usuario no banco com Spring Security*/
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
