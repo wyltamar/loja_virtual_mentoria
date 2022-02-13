675df00d972ab72f5a7a272c53a26b4ec3692bdf
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-02-13 00:00:11 -0300

    Exemplo de mais uma camada de segurança

--- src/main/java/mentoria/lojavirtual/controller/AcessoController.java
+++ src/main/java/mentoria/lojavirtual/controller/AcessoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,6 +47,7 @@ public class AcessoController {
 		return new ResponseEntity("Acesso excluido com suceso", HttpStatus.OK);
 	}
 	
+	//@Secured({"ROLE_GERENTE","ROLE_ADMIN"})
 	@ResponseBody
 	@DeleteMapping(value = "**/deleteAcessoPorId/{id}") 
 	public ResponseEntity<?> deleteAcessoPorId(@PathVariable ("id") Long id) {
