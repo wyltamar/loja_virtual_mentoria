c965417e9ea24e2e05bce80396b75ce98bd946a9
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-02-12 22:59:30 -0300

    Implementação do método de liberação de cors

--- src/main/java/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
+++ src/main/java/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -43,9 +43,29 @@ public class JWTTokenAutenticacaoService {
 		/*Dar a resposta para a tela e para outro cliente, ex. outra API, navegador, aplicativo, javascript, 
 		 * outra chamada java*/
 		response.addHeader(HEADER_STRING, token);
+		liberacaoCors(response);
 		
 		/*Usados para ver no Postman para teste*/
 		response.getWriter().write("{\"Autorization\": \"" + token + "\"}");
 	}
 	
+	/*Fazendo liberação contra erro de Cors no navegador*/
+	private void liberacaoCors(HttpServletResponse response) {
+		
+		if(response.getHeader("Access-Control-Allow-Origin") == null) {
+			response.addHeader("Access-Control-Allow-Origin", "*");
+		}
+		
+		if(response.getHeader("Access-Control-Allow-Headers") == null) {
+			response.addHeader("Access-Control-Allow-Headers", "*");
+		}
+		
+		if(response.getHeader("Access-Control-Request-Headers") == null) {
+			response.addHeader("Access-Control-Request-Headers", "*");
+		}
+		
+		if(response.getHeader("Access-Control-Allow-Methods") == null) {
+			response.addHeader("Access-Control-Allow-Methods", "*");
+		}
+	}
 }
