e4c743e923b51b18f3cd7c99bf20d4ec3011160e
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-02-13 21:28:37 -0300

    Inserindo a notação @Service na classe

--- src/main/java/mentoria/lojavirtual/service/ImplementacaoUserDetailsService.java
+++ src/main/java/mentoria/lojavirtual/service/ImplementacaoUserDetailsService.java
@@ -5,10 +5,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import mentoria.lojavirtual.model.Usuario;
 import mentoria.lojavirtual.repository.UsuarioRepository;
 
+@Service
 public class ImplementacaoUserDetailsService implements UserDetailsService{
 	
 	@Autowired
