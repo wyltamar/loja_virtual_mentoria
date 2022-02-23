feb1bc31c740f0c595edf12142cbec1bd829e4f5
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-02-22 22:15:24 -0300

    Controle de Exceções @RestControllerAdvice

---
+++ src/main/java/mentoria/lojavirtual/model/dto/ObjetoErroDTO.java
@@ -0,0 +1,26 @@
+package mentoria.lojavirtual.model.dto;
+
+import java.io.Serializable;
+
+public class ObjetoErroDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String error;
+	private String code;
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	
+
+}
