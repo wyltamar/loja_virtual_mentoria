ec6ddbc3ba9cb13fb1ccd34ad1a3b0a1fd208453
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-02-24 06:47:14 -0300

    Retirada de import desnecessário e adicionado <> return do metodo deleteAcessoPorId

--- src/main/java/mentoria/lojavirtual/controller/AcessoController.java
+++ src/main/java/mentoria/lojavirtual/controller/AcessoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,7 +43,7 @@ public class AcessoController {
 		
 		acessoService.delete(acesso);
 		
-		return new ResponseEntity("Acesso excluido com suceso", HttpStatus.OK);
+		return new ResponseEntity<>("Acesso excluido com suceso", HttpStatus.OK);
 	}
 	
 	//@Secured({"ROLE_GERENTE","ROLE_ADMIN"})
