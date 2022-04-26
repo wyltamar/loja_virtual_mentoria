976d54455ac985c907c87ade42204c9936d573d1
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-04-26 05:38:31 -0300

    Inserção do modificador de acesso Public na método buscarAcesso

--- src/main/java/mentoria/lojavirtual/repository/AcessoRepository.java
+++ src/main/java/mentoria/lojavirtual/repository/AcessoRepository.java
@@ -14,6 +14,6 @@ import mentoria.lojavirtual.model.Acesso;
 public interface AcessoRepository extends JpaRepository<Acesso, Long> {
 
 	@Query("select a from Acesso a where upper(trim(a.descricao)) like %?1%")
-	List<Acesso> buscarAcessoDesc(String desc);
+	public List<Acesso> buscarAcessoDesc(String desc);
 		
 }
