40b0f5cdd07ee2a7d002a8ab22642a7ba6a2956b
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-06-02 23:47:02 -0300

    Retirada de relacionamento de nota item com produto

--- src/main/java/mentoria/lojavirtual/model/NotaItemProduto.java
+++ src/main/java/mentoria/lojavirtual/model/NotaItemProduto.java
@@ -38,16 +38,16 @@ public class NotaItemProduto implements Serializable {
 	@ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
 	private Produto produto;
 	
-	@ManyToOne(targetEntity = Pessoa.class)
+	@ManyToOne(targetEntity = PessoaJuridica.class)
 	@JoinColumn(name = "empresa_id", nullable = false, foreignKey = 
 	@ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
-	private Pessoa empresa;
+	private PessoaJuridica empresa;
 
-	public Pessoa getEmpresa() {
+	public PessoaJuridica getEmpresa() {
 		return empresa;
 	}
 
-	public void setEmpresa(Pessoa empresa) {
+	public void setEmpresa(PessoaJuridica empresa) {
 		this.empresa = empresa;
 	}
 
