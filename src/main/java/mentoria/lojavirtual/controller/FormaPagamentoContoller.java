cde487fca7c8f2250f0a8448707dbf47296c5987
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-08-06 09:41:44 -0300

    Criação do endpoint para carregar a lista de formas de pagamento

--- src/main/java/mentoria/lojavirtual/controller/FormaPagamentoContoller.java
+++ src/main/java/mentoria/lojavirtual/controller/FormaPagamentoContoller.java
@@ -1,10 +1,13 @@
 package mentoria.lojavirtual.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,6 +30,16 @@ public class FormaPagamentoContoller {
 		formaPagamento = formaPagamentoRepository.saveAndFlush(formaPagamento);
 		
 		return new ResponseEntity<FormaPagamento>(formaPagamento, HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@GetMapping(value = "**/listarFormasDePagamento")
+	public ResponseEntity<List<FormaPagamento>>listarFormasDePagamento(){
+		
+		List<FormaPagamento> formasPagamento = formaPagamentoRepository.listarFormasDePagamento();
+		
+		return new ResponseEntity<List<FormaPagamento>>(formasPagamento, HttpStatus.OK);
+		
 	}
 	
 
