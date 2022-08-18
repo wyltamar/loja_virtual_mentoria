17871a2ec72bd0e9f831ac2c7aa57b9afcfdf28f
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-08-18 07:19:31 -0300

    Correçao no pessoa juridica

--- src/main/java/mentoria/lojavirtual/repository/PessoaRepository.java
+++ src/main/java/mentoria/lojavirtual/repository/PessoaRepository.java
@@ -1,5 +1,7 @@
 package mentoria.lojavirtual.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -14,6 +16,9 @@ public interface PessoaRepository extends CrudRepository<PessoaJuridica, Long> {
 	@Query(value = "select pj from PessoaJuridica pj where pj.cnpj = ?1")
 	public PessoaJuridica existeCnpjCadastrado(String cnpj);
 	
+	@Query(value = "select pj from PessoaJuridica pj where pj.cnpj = ?1")
+	public List<PessoaJuridica> existeCnpjCadastradoList(String cnpj);
+	
 	@Query(value = "select pf from PessoaFisica pf where pf.cpf = ?1")
 	public PessoaFisica existeCpfCadastrado(String cpf);
 	
@@ -24,5 +29,7 @@ public interface PessoaRepository extends CrudRepository<PessoaJuridica, Long> {
 	@Query(value = "select u from Usuario u where u.pessoa.id = ?1 or u.login = ?2")
 	public Usuario findUserByPessoa(Long id, String email);
 
+	@Query(value = "selec pj from PessoaJuridica pj where pj.nome like %?1%")
+	public List<PessoaJuridica> pesquisaPorNomePJ(String nome);
 	
 }
