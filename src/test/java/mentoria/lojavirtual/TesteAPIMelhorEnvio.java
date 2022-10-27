03504cbafdb443bb552789230f256ddbdc18cc98
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-10-26 22:36:03 -0300

    Testes de integrção com a API da MelhorEnvio

--- src/test/java/mentoria/lojavirtual/TesteAPIMelhorEnvio.java
+++ src/test/java/mentoria/lojavirtual/TesteAPIMelhorEnvio.java
@@ -1,15 +1,7 @@
 package mentoria.lojavirtual;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import mentoria.lojavirtual.enums.ApiTokenIntegracao;
-import mentoria.lojavirtual.model.dto.EmpresaTransporteDTO;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -21,60 +13,92 @@ public class TesteAPIMelhorEnvio {
 	
 	public static void main(String[] args) throws IOException {
 		
+		/*Teste de insersão de fretes no carrinho*/
+		
+	/*	OkHttpClient client = new OkHttpClient().newBuilder()
+				  .build();
+				MediaType mediaType = MediaType.parse("application/json");
+				RequestBody body = RequestBody.create(mediaType, "{\n    \"service\": 3,\n    \"agency\": 49,\n    \"from\": {\n        \"name\": \"Nome do remetente\",\n        \"phone\": \"53984470102\",\n        \"email\": \"contato@melhorenvio.com.br\",\n        \"document\": \"16571478358\",\n        \"company_document\": \"89794131000100\",\n        \"state_register\": \"123456\",\n        \"address\": \"Endereço do remetente\",\n        \"complement\": \"Complemento\",\n        \"number\": \"1\",\n        \"district\": \"Bairro\",\n        \"city\": \"São Paulo\",\n        \"country_id\": \"BR\",\n        \"postal_code\": \"01002001\",\n        \"note\": \"observação\"\n    },\n    \"to\": {\n        \"name\": \"Nome do destinatário\",\n        \"phone\": \"53984470102\",\n        \"email\": \"contato@melhorenvio.com.br\",\n        \"document\": \"25404918047\",\n        \"company_document\": \"07595604000177\",\n        \"state_register\": \"123456\",\n        \"address\": \"Endereço do destinatário\",\n        \"complement\": \"Complemento\",\n        \"number\": \"2\",\n        \"district\": \"Bairro\",\n        \"city\": \"Porto Alegre\",\n        \"state_abbr\": \"RS\",\n        \"country_id\": \"BR\",\n        \"postal_code\": \"90570020\",\n        \"note\": \"observação\"\n    },\n    \"products\": [\n        {\n            \"name\": \"Papel adesivo para etiquetas 1\",\n            \"quantity\": 3,\n            \"unitary_value\": 100.00\n        },\n        {\n            \"name\": \"Papel adesivo para etiquetas 2\",\n            \"quantity\": 1,\n            \"unitary_value\": 700.00\n        }\n    ],\n    \"volumes\": [\n        {\n            \"height\": 15,\n            \"width\": 20,\n            \"length\": 10,\n            \"weight\": 3.5\n        }\n    ],\n    \"options\": {\n        \"insurance_value\": 1000.00,\n        \"receipt\": false,\n        \"own_hand\": false,\n        \"reverse\": false,\n        \"non_commercial\": false,\n        \"invoice\": {\n            \"key\": \"31190307586261000184550010000092481404848162\"\n        },\n        \"platform\": \"Nome da Plataforma\",\n        \"tags\": [\n            {\n                \"tag\": \"Identificação do pedido na plataforma, exemplo: 1000007\",\n                \"url\": \"Link direto para o pedido na plataforma, se possível, caso contrário pode ser passado o valor null\"\n            }\n        ]\n    }\n}");
+				Request request = new Request.Builder()
+				  .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX +"api/v2/me/cart")
+				  .method("POST", body)
+				  .addHeader("Accept", "application/json")
+				  .addHeader("Content-Type", "application/json")
+				  .addHeader("Authorization", "Bearer "+ ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
+				  .addHeader("User-Agent", "wyltamarjavadev@gmail.com")
+				  .build();
+				Response response = client.newCall(request).execute();
+				
+				System.out.println(response.body().string()); */
+		
+		/*teste compra de frete ou Chekout de ordens*/
+		/*
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				  .build();
+				MediaType mediaType = MediaType.parse("application/json");
+				RequestBody body = RequestBody.create(mediaType, "{\n    \"orders\": [\n        \"78417fae-eda6-4808-8700-93a24d919c10\"\n    ]\n}");
+				Request request = new Request.Builder()
+				  .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX +"api/v2/me/shipment/checkout")
+				  .method("POST", body)
+				  .addHeader("Accept", "application/json")
+				  .addHeader("Content-Type", "application/json")
+				  .addHeader("Authorization", "Bearer "+ ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
+				  .addHeader("User-Agent", "wyltamarjavadev@gmail.com")
+				  .build();
+				Response response = client.newCall(request).execute();
+				System.out.println(response.body().string()); */
+		
+		/*Teste de pre-vizualização de etiquetas*/
+		/*
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				  .build();
+				MediaType mediaType = MediaType.parse("application/json");
+				RequestBody body = RequestBody.create(mediaType, "{\n    \"orders\": [\n        \"78417fae-eda6-4808-8700-93a24d919c10\"\n    ]\n}");
+				Request request = new Request.Builder()
+				  .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX+"api/v2/me/shipment/preview")
+				  .method("POST", body)
+				  .addHeader("Accept", "application/json")
+				  .addHeader("Content-Type", "application/json")
+				  .addHeader("Authorization", "Bearer "+ ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
+				  .addHeader("User-Agent", "wyltamarjavadev@gmail.com")
+				  .build();
+				Response response = client.newCall(request).execute();
+				System.out.println(response.body().string()); */
+		
+		
+		/*Teste de geração de etiquetas*/
+		/*
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				  .build();
+				MediaType mediaType = MediaType.parse("application/json");
+				RequestBody body = RequestBody.create(mediaType, "{\n    \"orders\":[\n        \"78417fae-eda6-4808-8700-93a24d919c10\"\n    ]\n}");
+				Request request = new Request.Builder()
+				  .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX+"api/v2/me/shipment/generate")
+				  .method("POST", body)
+				  .addHeader("Accept", "application/json")
+				  .addHeader("Content-Type", "application/json")
+				  .addHeader("Authorization", "Bearer "+ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
+				  .addHeader("User-Agent", "wyltamarjavadev@gmail.com")
+				  .build();
+				Response response = client.newCall(request).execute();
+				System.out.println(response.body().string()); */
+		
+		/*Teste de impressão da etiqueta*/
 		
 		OkHttpClient client = new OkHttpClient().newBuilder()
 				  .build();
 				MediaType mediaType = MediaType.parse("application/json");
-				RequestBody body = RequestBody.create(mediaType, "{\n    \"from\": {\n        \"postal_code\": \"96020360\"\n    },\n    \"to\": {\n        \"postal_code\": \"01018020\"\n    },\n    \"products\": [\n        {\n            \"id\": \"x\",\n            \"width\": 11,\n            \"height\": 17,\n            \"length\": 11,\n            \"weight\": 0.3,\n            \"insurance_value\": 10.1,\n            \"quantity\": 1\n        },\n        {\n            \"id\": \"y\",\n            \"width\": 16,\n            \"height\": 25,\n            \"length\": 11,\n            \"weight\": 0.3,\n            \"insurance_value\": 55.05,\n            \"quantity\": 2\n        },\n        {\n            \"id\": \"z\",\n            \"width\": 22,\n            \"height\": 30,\n            \"length\": 11,\n            \"weight\": 1,\n            \"insurance_value\": 30,\n            \"quantity\": 1\n        }\n    ]\n}");
+				RequestBody body = RequestBody.create(mediaType, "{\n    \"mode\": \"private\",\n    \"orders\": [\n        \"78417fae-eda6-4808-8700-93a24d919c10\"\n    ]\n}");
 				Request request = new Request.Builder()
-				  .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX+"/api/v2/me/shipment/calculate")
+				  .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX+"api/v2/me/shipment/print")
 				  .method("POST", body)
 				  .addHeader("Accept", "application/json")
 				  .addHeader("Content-Type", "application/json")
-				  .addHeader("Authorization", "Bearer " +ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
+				  .addHeader("Authorization", "Bearer "+ ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
 				  .addHeader("User-Agent", "wyltamarjavadev@gmail.com")
 				  .build();
 				Response response = client.newCall(request).execute();
-				//System.out.println(response.body().string());
-				
-				JsonNode jsonNode = new ObjectMapper().readTree(response.body().string());
-				
-				Iterator<JsonNode> iterator = jsonNode.iterator();
-				
-				List<EmpresaTransporteDTO> empresaTransporteDTOList = new ArrayList<EmpresaTransporteDTO>();
-				
-				while(iterator.hasNext()) {
-					JsonNode node = iterator.next();
-					
-					EmpresaTransporteDTO empresaTransporteDTO = new EmpresaTransporteDTO();
-					
-					if(node.get("id") != null) {
-						empresaTransporteDTO.setId(node.get("id").asText()); 
-					}
-					
-					if(node.get("name") != null) {
-						empresaTransporteDTO.setNome(node.get("name").asText());
-					}
-					
-					if(node.get("price") != null) {
-						empresaTransporteDTO.setValor(node.get("price").asText());
-					}
-					
-					if(node.get("company").get("name") != null) {
-						empresaTransporteDTO.setEmpresa(node.get("company").get("name").asText());
-					}
-					
-					if(node.get("company").get("picture") != null) {
-						empresaTransporteDTO.setPicture(node.get("company").get("picture").asText());
-					}
-					
-					if(empresaTransporteDTO.dadosOK()) {
-						empresaTransporteDTOList.add(empresaTransporteDTO);
-					}
-					
-				}
-				
-				System.out.println(empresaTransporteDTOList);
+				System.out.println(response.body().string());
 				
 	}
 
